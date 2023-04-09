4eb88808d9f3993bb4bf75015bdf812da6190047
tororosoba0534 <tekuteku11udana888@gmail.com>
2023-04-09 10:05:47 +0900

    nvim: setup lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- lazy.nvim -- plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "mbbill/undotree"
+})
+
+
 vim.o.scrolloff = 10
 vim.o.wrap = true
 vim.o.showbreak = '>>>'
