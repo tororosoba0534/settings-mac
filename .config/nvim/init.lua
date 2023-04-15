56f07f8a006c8039bd689e1725ed619d227c3641
tororosoba0534 <tekuteku11udana888@gmail.com>
2023-04-15 22:32:44 +0900

    nvim: introduce nvim-treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 })
 
 -- LSP settings
@@ -194,6 +195,21 @@ null_ls.setup({
 -- 		"less",
 -- 	},
 -- })
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript" },
+	auto_install = false,
+	highlight = {
+		enable = true,
+		disable = function(_, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+	},
+})
 
 require("nvim-web-devicons").setup({})
 
