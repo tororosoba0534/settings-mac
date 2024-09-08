3f66a8c164814dff6c4ec8557a4ea01457c10079
tororosoba0534 <tekuteku11udana888@gmail.com>
2024-09-09 04:22:03 +0900

    nvim-cmp: update command-line source

--- .config/nvim/lua/lazy-configs/nvim-cmp.lua
+++ .config/nvim/lua/lazy-configs/nvim-cmp.lua
@@ -34,13 +34,6 @@ export.dependencies = {
 }
 
 local mapping = function(cmp)
-	local has_words_before = function()
-		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-			return false
-		end
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-	end
 	return {
 		-- CAUTION: cmp.foo is NOT equals to cmp.mapping.foo
 		['<CR>'] = {
@@ -203,16 +196,15 @@ export.config = function()
 	})
 
 	cmp.setup.cmdline(":", {
-		sources = cmp.config.sources({
+		sources = {
 			{ name = "path" },
-		}, {
 			{
 				name = "cmdline",
 				option = {
 					ignoer_cmds = { "Man", "!" }
 				},
 			}
-		}),
+		},
 		completion = {
 			autocomplete = false
 		},
