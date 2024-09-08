662356b1e0eb6433b41c743ee0268bcb696bc490
tororosoba0534 <tekuteku11udana888@gmail.com>
2024-09-08 10:06:15 +0900

    nvim: update nvim-cmp settings for copilot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -151,6 +151,13 @@ require("lazy").setup({
 		event = { "InsertEnter" },
 		config = function()
 			local cmp = require("cmp")
+			local has_words_before = function()
+				if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+					return false
+				end
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+			end
 			cmp.setup.global({
 				snippet = {
 					expand = function(args)
@@ -170,6 +177,13 @@ require("lazy").setup({
 					['<C-n>'] = cmp.mapping.select_next_item(),
 					['<Up>'] = cmp.mapping.select_prev_item(),
 					['<Down>'] = cmp.mapping.select_next_item(),
+					['<Tab>'] = vim.schedule_wrap(function(fallback)
+						if cmp.visible() and has_words_before() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						else
+							fallback()
+						end
+					end),
 				},
 				sources = {
 					{
