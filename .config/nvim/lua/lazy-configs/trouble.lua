45e1f09d9c20cafe39b5d9962f94d74f7b5b1244
tororosoba0534 <tekuteku11udana888@gmail.com>
2024-09-13 22:31:45 +0900

    nvim: update trouble.nvim settings

--- .config/nvim/lua/lazy-configs/trouble.lua
+++ .config/nvim/lua/lazy-configs/trouble.lua
@@ -4,13 +4,61 @@ export.dependencies = "nvim-tree/nvim-web-devicons"
 export.lazy = true
 
 export.init = function()
-	vim.api.nvim_create_user_command("E", "Trouble diagnostics toggle focus=true", {})
+	vim.api.nvim_create_user_command("E", "TroubleToggleMyMenuCurrent", {})
+	vim.api.nvim_create_user_command("Ea", "TroubleToggleMyMenuAll", {})
 end
 
-export.cmd = { 'Trouble' }
+export.cmd = { 'Trouble', 'TroubleToggleMyMenuCurrent', 'TroubleToggleMyMenuAll' }
 
 export.config = function()
-	require("trouble").setup({})
+	local trouble = require("trouble")
+
+	trouble.setup({
+		modes = {
+			current = {
+				mode = "diagnostics",
+				focus = true,
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+				filter = { buf = 0 }
+			},
+			all = {
+				mode = "diagnostics",
+				focus = true,
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+			},
+		},
+	})
+
+	local toggle_current = function()
+		if trouble.is_open('all') then
+			trouble.close('all')
+			trouble.open('current')
+		else
+			trouble.toggle('current')
+		end
+	end
+
+	local toggle_all = function()
+		if trouble.is_open('current') then
+			trouble.close('current')
+			trouble.open('all')
+		else
+			trouble.toggle('all')
+		end
+	end
+
+	vim.api.nvim_create_user_command("TroubleToggleMyMenuCurrent", toggle_current, {})
+	vim.api.nvim_create_user_command("TroubleToggleMyMenuAll", toggle_all, {})
 end
 
 return export
