4704096c83275f66170a822ca52c5f8271c9c05f
tororosoba0534 <tekuteku11udana888@gmail.com>
2024-11-11 23:27:04 +0900

    telescope.nvim: delete redundant custom command

--- .config/nvim/lua/lazy-configs/telescope.lua
+++ .config/nvim/lua/lazy-configs/telescope.lua
@@ -16,26 +16,66 @@ export.lazy = true
 
 export.init = function()
 	-- Find file
-	vim.keymap.set({ 'n' }, '<Leader>f', "<CMD>Telescope find_files<CR>", {})
+	vim.keymap.set({ 'n' }, '<Leader>f', function()
+		require("telescope.builtin").find_files()
+	end, {})
 	-- Find text
-	vim.keymap.set({ 'n' }, '<Leader>t', "<CMD>TelescopeLiveGrepArgs<CR>", {})
-	vim.keymap.set({ 'x' }, '<Leader>t', "<CMD>TelescopeLiveGrepArgsSelected<CR>", {})
+	vim.keymap.set({ 'n' }, '<Leader>t', function()
+		require("telescope.builtin").live_grep { glob_pattern = '!.git' }
+	end, {})
+	vim.keymap.set({ 'x' }, '<Leader>t', function()
+		local text = require('utils.selection-getter').get_texts()[1]
+		vim.cmd("noh") -- Workaround for deleting remained selection highlight
+		require("telescope.builtin").live_grep { default_text = text, glob_pattern = '!.git' }
+	end, {})
 	-- Find help page
-	vim.keymap.set('n', '<Leader>h', '<CMD>TelescopeMyHelpPage<CR>', {})
-	vim.keymap.set('x', '<Leader>h', '<CMD>TelescopeMyHelpPageSelected<CR>', {})
+	vim.keymap.set('n', '<Leader>h', function()
+		require('custom.telescope').pick_help_page {}
+	end, {})
+	vim.keymap.set('x', '<Leader>h', function()
+		local text = require('utils.selection-getter').get_texts()[1]
+		vim.cmd("noh") -- Workaround for deleting remained selection highlight
+		require('custom.telescope').pick_help_page { default_text = text }
+	end, {})
 	-- Find buffer
-	vim.keymap.set({ 'n' }, '<Leader>b', '<CMD>TelescopeMyBuffers!<CR>', {})
+	vim.keymap.set({ 'n' }, '<Leader>b', function()
+		local actions = require("telescope.actions")
+		require("telescope.builtin").buffers {
+			attach_mappings = function(_, map)
+				local delete_selected_buffers = function(_prompt_bufnr)
+					local base_bufnr = vim.fn.bufnr('#')
+					local deleting_base_buffer = false
+					local current_picker = require("telescope.actions.state").get_current_picker(_prompt_bufnr)
+					current_picker:delete_selection(function(selection)
+						if selection.bufnr == base_bufnr then
+							deleting_base_buffer = true
+						else
+							vim.api.nvim_buf_delete(selection.bufnr, { force = false })
+						end
+					end)
+					if deleting_base_buffer then
+						actions.close(_prompt_bufnr)
+						vim.api.nvim_buf_delete(base_bufnr, { force = false })
+						vim.cmd("TelescopeMyBuffers!")
+					end
+				end
+				map({ "n" }, "<TAB>", actions.toggle_selection)
+				map({ "n" }, "d", delete_selected_buffers)
+				map({ "n" }, "D", function(_prompt_bufnr)
+					actions.add_selection(_prompt_bufnr)
+					actions.toggle_all(_prompt_bufnr)
+					delete_selected_buffers(_prompt_bufnr)
+				end)
+				return true
+			end,
+		}
+	end, {})
 end
-
-export.cmd = { "Telescope", "TelescopeMyBuffers", "TelescopeLiveGrepArgs", "TelescopeLiveGrepArgsSelected",
-	"TelescopeMyHelpPage",
-	"TelescopeMyHelpPageSelected" }
 
 export.config = function()
 	local telescope = require("telescope")
 	require("telescope").load_extension("live_grep_args")
 	local actions = require("telescope.actions")
-	local action_state = require("telescope.actions.state")
 	telescope.setup({
 		defaults = {
 			file_ignore_patterns = { "node_modules", ".git" },
@@ -79,60 +119,6 @@ export.config = function()
 			live_grep_args = {},
 		},
 	})
-
-	vim.api.nvim_create_user_command("TelescopeMyBuffers", function(opts)
-		local entering_normal_mode = opts.bang
-		require("telescope.builtin").buffers {
-			-- on_complete = { function()
-			-- 	if entering_normal_mode then
-			-- 		vim.cmd("stopinsert")
-			-- 	end
-			-- end },
-			attach_mappings = function(_, map)
-				local delete_selected_buffers = function(_prompt_bufnr)
-					local base_bufnr = vim.fn.bufnr('#')
-					local deleting_base_buffer = false
-					local current_picker = action_state.get_current_picker(_prompt_bufnr)
-					current_picker:delete_selection(function(selection)
-						if selection.bufnr == base_bufnr then
-							deleting_base_buffer = true
-						else
-							vim.api.nvim_buf_delete(selection.bufnr, { force = false })
-						end
-					end)
-					if deleting_base_buffer then
-						actions.close(_prompt_bufnr)
-						vim.api.nvim_buf_delete(base_bufnr, { force = false })
-						vim.cmd("TelescopeMyBuffers!")
-					end
-				end
-				map({ "n" }, "<TAB>", actions.toggle_selection)
-				map({ "n" }, "d", delete_selected_buffers)
-				map({ "n" }, "D", function(_prompt_bufnr)
-					actions.add_selection(_prompt_bufnr)
-					actions.toggle_all(_prompt_bufnr)
-					delete_selected_buffers(_prompt_bufnr)
-				end)
-				return true
-			end,
-		}
-	end, { bang = true })
-	vim.api.nvim_create_user_command("TelescopeLiveGrepArgs", function()
-		require("telescope.builtin").live_grep { glob_pattern = '!.git' }
-	end, {})
-	vim.api.nvim_create_user_command("TelescopeLiveGrepArgsSelected", function()
-		local text = require('utils.selection-getter').get_texts()[1]
-		vim.cmd("noh") -- Workaround for deleting remained selection highlight
-		require("telescope.builtin").live_grep { default_text = text, glob_pattern = '!.git' }
-	end, {})
-	vim.api.nvim_create_user_command("TelescopeMyHelpPage", function()
-		require('custom.telescope').pick_help_page {}
-	end, {})
-	vim.api.nvim_create_user_command("TelescopeMyHelpPageSelected", function()
-		local text = require('utils.selection-getter').get_texts()[1]
-		vim.cmd("noh") -- Workaround for deleting remained selection highlight
-		require('custom.telescope').pick_help_page { default_text = text }
-	end, {})
 end
 
 return export
